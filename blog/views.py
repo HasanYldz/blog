a63b9e06fb3c17c890a36d671bc2d2d595b9e2d1
HasanYldz <hasanyldz70@gmail.com>
2022-02-15 10:36:36 +0300

    template

--- blog/views.py
+++ blog/views.py
@@ -8,9 +8,9 @@ from .models import Post
 class index(View):
     def get(self, request):
         posts = Post.objects.filter(status=1)
-        return render(request, 'index.html', {'posts':posts})
+        return render(request, 'blog/index.html', {'posts':posts})
 
 class post_detail(View):
     def get(self, request, slug):
         post = get_object_or_404(Post, slug=slug)
-        return render(request, 'post_detail.html', {'post':post})
+        return render(request, 'blog/post_detail.html', {'post':post})
--- new_blog/settings.py
+++ new_blog/settings.py
@@ -80,10 +80,6 @@ WSGI_APPLICATION = 'new_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'TEST': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'tesst.db',
-        },
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog_db',
         'USER': 'hasan',
@@ -130,7 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'checkstatic/static/')
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
